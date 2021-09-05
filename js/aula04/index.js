b08dced3de9a794fca503ebcfa736f0e73c1c8a0
rubensabimael <rubensabimael26@gmail.com>
2021-09-05 09:10:38 -0300

    Eu aprindir com usar o conetario.

---
+++ js/aula04/index.js
@@ -0,0 +1,12 @@
+// E escrevar um comentário 
+console.log('Olá mundo!'); // Aqui tem outro comentário 
+console.log('Linha 1');
+// Comentário
+console.log('Linha 2');
+console.log('Linha 3');
+// Vai ser ignorado
+
+/**
+ * Um bloco de código
+ * Linha de comentário
+ */
